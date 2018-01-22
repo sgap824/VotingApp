ea964d00cbc61d2e2414ecd2b97affe63466985a
JIALIN LIU <jialin824@gmail.com>
2018-01-22 21:00:34 +1100

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,60 @@
+class Product extends React.Component{
+  render(){
+    return(
+      <div className='item'>
+        <div className='image'>
+          <img src= {this.props.productImageUrl}/>
+        </div>
+        <div className="middle aligned content">
+          <div className="header">
+            <a>
+              <i className="large caret up icon"/>
+            </a>
+            {this.props.votes}
+          </div>
+          <div className="discription">
+            <a href={this.props.url}>
+              {this.props.title}
+            </a>
+            <p>
+              {this.props.discription}
+            </p>
+          </div>
+          <div className="extra">
+            <span>Submitted by: </span>
+            <img className="ui avatar image"
+            src={this.props.submitterAvatarUrl}/>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+class ProductList extends React.Component{
+  render(){
+    const products = Seed.products.sort((a, b) => (b.votes - a.votes));
+    const productComponents = products.map((product) => (
+        <Product
+        key={"product-" + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}/>
+    ));
+    return(
+        <div className='ui unstackable items'>
+          {productComponents}
+        </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <ProductList/>,
+  document.getElementById('content')
+);
